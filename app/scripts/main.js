8db914641bcf3ba03448511da8d46014fd29c1cc
Ian McGaunn <ian_mcgaunn@student.uml.edu>
2014-11-06 15:39:07 +0000

    intial working commit

---
+++ app/scripts/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+(function() {
+
+  function getContent(ID) {
+    var url = 'partials/' + ID + '.part';
+    $.get(url)
+      .success(function(data) {
+        $("#dynamicContent").html(data);
+      })
+      .error(function() {
+        $("#dynamicContent").html('<p class="error"> page ' + ID + ' missing </p>');
+      });
+  }
+
+  function navigate() {
+    // strip the hash from the name
+    var ID = location.hash.substr(1);
+    console.log(ID);
+
+    // set the active class for the proper link
+    $('#navbar a').removeClass('active');
+    console.log('navbar a[href=#' + ID + ']');
+    $('#navbar a[href="#' + ID + '"]').addClass('active');
+
+    // load the partial associated with ID
+    getContent(ID);
+  }
+
+
+  if (!location.hash) {
+    location.hash = '#home';
+  }
+
+  // respond to hashchange event
+  $(window).on('hashchange', navigate);
+})();
